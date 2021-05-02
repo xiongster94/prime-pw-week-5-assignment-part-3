299047b55593ed6da0ababc8ff4fd5114c8f313e
Jerry Xiong <xion0597@umn.edu>
2021-05-02 11:59:13 -0500

    created addToCollection function, tested for error

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,3 +1,16 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+  let newSong = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  } // create new object
+  return collection.unshift(newSong);
+} // end addToCollection
+
+addToCollection ('Here Comes The Sun', 'The Beatles', 1969);
+
+console.log(collection);
