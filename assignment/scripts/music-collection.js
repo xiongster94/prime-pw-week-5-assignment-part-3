8ba0588e8683232ac52493e463daa8f7b2d1bbdc
Jerry Xiong <xion0597@umn.edu>
2021-05-02 13:57:07 -0500

    created and tested showCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,6 +2,7 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+// create  addToCollection function
 function addToCollection (title, artist, yearPublished){
   let newSong = {
     title: title,
@@ -12,6 +13,7 @@ function addToCollection (title, artist, yearPublished){
   return collection[collection.length -1];
 } // end addToCollection
 
+// adding songs
 console.log('Added', addToCollection ('Here Comes The Sun', 'The Beatles', 1969));
 console.log('Added', addToCollection ('Hey Jude', 'The Beatles', 1968));
 console.log('Added', addToCollection ('Hey Ya!', 'OutKast', 2003));
@@ -20,3 +22,16 @@ console.log('Added', addToCollection ('Best Day Of My Life', 'American Authors',
 console.log('Added', addToCollection ('I Gotta Feeling', 'Black Eyed Peas', 2009));
 
 console.log(collection);
+// testing addToCollection function
+
+// create showCollection function
+function showCollection(array){
+  console.log(`Songs in collection: ${array.length}`);
+  for (let i = 0; i < array.length; i++) {
+    let values = Object.values(array[i]);
+      // creating parameter for values in array
+    console.log(`${values[0]} by ${values[1]}, published in ${values[2]}`);
+  } // end loop for each value found in each array[i]
+} // end show collection function
+
+showCollection(collection);
