72be9063d864f32ad767166f7168b88a899b9246
Jerry Xiong <xion0597@umn.edu>
2021-05-02 12:35:06 -0500

    Fixed addToCollection function to return correct object. Also added 6 new objects to collection array along with console logs

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -7,10 +7,16 @@ function addToCollection (title, artist, yearPublished){
     title: title,
     artist: artist,
     yearPublished: yearPublished,
-  } // create new object
-  return collection.unshift(newSong);
+    } // create new object && add to array
+  collection.push(newSong);
+  return collection[collection.length -1];
 } // end addToCollection
 
-addToCollection ('Here Comes The Sun', 'The Beatles', 1969);
+console.log('Added', addToCollection ('Here Comes The Sun', 'The Beatles', 1969));
+console.log('Added', addToCollection ('Hey Jude', 'The Beatles', 1968));
+console.log('Added', addToCollection ('Hey Ya!', 'OutKast', 2003));
+console.log('Added', addToCollection ('Where Is The Love?', 'Black Eyed Peas', 2003));
+console.log('Added', addToCollection ('Best Day Of My Life', 'American Authors', 2014));
+console.log('Added', addToCollection ('I Gotta Feeling', 'Black Eyed Peas', 2009));
 
 console.log(collection);
